104f9ed1d8b21810cd95135d05abd354c3690f3a
Matteo Castellucci <matteo.castellucci@outlook.com>
2022-12-14 16:01:32 +0100

    build: add refined library dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,4 +3,5 @@ import sbt._
 object Dependencies {
   lazy val scalactic: ModuleID = "org.scalactic" %% "scalactic" % "3.2.14"
   lazy val scalatest: ModuleID = "org.scalatest" %% "scalatest" % "3.2.14" % Test
+  lazy val refined: ModuleID = "eu.timepit" %% "refined" % "0.10.1"
 }
