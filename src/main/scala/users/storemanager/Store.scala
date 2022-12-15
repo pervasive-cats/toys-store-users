05c09127917d54bde1c1f36a7517b33562f22c55
Elena Rughi <elenarughi@gmail.com>
2022-12-15 19:32:43 +0100

    fix: scalafix formatting

--- src/main/scala/users/storemanager/Store.scala
+++ src/main/scala/users/storemanager/Store.scala
@@ -7,9 +7,9 @@
 package io.github.pervasivecats
 package users.storemanager
 
-import eu.timepit.refined.auto.given
 import eu.timepit.refined.api.RefType.applyRef
 import eu.timepit.refined.api.Refined
+import eu.timepit.refined.auto.given
 import eu.timepit.refined.numeric.NonNegative
 
 import users.{Validated, ValidationError}
