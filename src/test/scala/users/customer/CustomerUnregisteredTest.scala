d9a330f95c0aa3ed66cd6e164af64cf9a3cfe923
Matteo Castellucci <matteo.castellucci@outlook.com>
2023-01-17 23:00:41 +0100

    feat: add CustomerUnregistered domain event

---
+++ src/test/scala/users/customer/CustomerUnregisteredTest.scala
@@ -0,0 +1,29 @@
+/*
+ * Copyright © 2022-2023 by Pervasive Cats S.r.l.s.
+ *
+ * All Rights Reserved.
+ */
+
+package io.github.pervasivecats
+package users.customer
+
+import eu.timepit.refined.auto.given
+import org.scalatest.EitherValues.given
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers.*
+
+import users.customer.events.CustomerUnregistered
+import users.customer.valeuobjects.Email
+
+class CustomerUnregisteredTest extends AnyFunSpec {
+
+  describe("A customer unregistered event") {
+    describe("when created with a customer email") {
+      it("should contain it") {
+        val email: Email = Email("example@mail.com").getOrElse(fail())
+
+        CustomerUnregistered(email).email shouldBe email
+      }
+    }
+  }
+}
