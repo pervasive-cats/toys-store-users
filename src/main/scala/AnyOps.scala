44f7d645386d842620ab18e8b5166363fd1d9cca
Matteo Castellucci <matteo.castellucci@outlook.com>
2023-01-17 22:34:54 +0100

    fix: add module for equals operator without cooperative equality

---
+++ src/main/scala/AnyOps.scala
@@ -0,0 +1,23 @@
+/*
+ * Copyright © 2022-2023 by Pervasive Cats S.r.l.s.
+ *
+ * All Rights Reserved.
+ */
+
+package io.github.pervasivecats
+
+import scala.annotation.targetName
+import scala.annotation.unused
+
+object AnyOps {
+
+  extension [A](self: A) {
+
+    @targetName("equals")
+    @SuppressWarnings(Array("org.wartremover.warts.Equals", "scalafix:DisableSyntax.=="))
+    def ===(other: A): Boolean = self == other
+
+    @targetName("notEquals")
+    def !==(other: A): Boolean = !(self === other)
+  }
+}
