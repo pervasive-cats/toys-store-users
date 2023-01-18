71c95cb57a700ceb0fed5242249c93172aa310ed
Ismam Abu <ismam.abu@studio.unibo.it>
2023-01-18 19:32:16 +0100

    feat: add findByUsername test

--- src/test/scala/users/administration/AdministrationRepositoryTest.scala
+++ src/test/scala/users/administration/AdministrationRepositoryTest.scala
@@ -9,11 +9,14 @@ import org.testcontainers.utility.DockerImageName
 import org.scalatest.funspec.AnyFunSpec
 import org.scalatest.matchers.should.Matchers.*
 import com.zaxxer.hikari.{HikariConfig, HikariDataSource}
+
 import java.sql.DriverManager
 import scala.concurrent.duration.{FiniteDuration, SECONDS}
-
 import users.user.valueobjects.{EncryptedPassword, Username}
 import users.ValidationError
+
+import io.github.pervasivecats.users.administration.AdministrationRepositoryError.UserNotFound
+import io.github.pervasivecats.users.administration.entities.Administration
 
 class AdministrationRepositoryTest  extends AnyFunSpec with TestContainerForAll{
 
@@ -49,6 +52,36 @@ class AdministrationRepositoryTest  extends AnyFunSpec with TestContainerForAll{
           Class.forName(pgContainer.driverClassName)
           val connection = DriverManager.getConnection(pgContainer.jdbcUrl, pgContainer.username, pgContainer.password)
           assert(!connection.isClosed)
+        }
+      }
+    }
+  }
+
+  describe("when asked to retrieve the administration corresponding to a username") {
+    it("should return the requested administration") {
+      withContainers { pgContainer =>
+        for {
+          username <- Username(usernameString)
+        } do {
+          AdministrationRepository.getInstance() match {
+            case Some(v) =>
+              val r = v.findByUsername(username)
+              (r.getOrElse(fail()).username.value: String) shouldBe usernameString
+            case None => fail()
+          }
+        }
+      }
+    }
+  }
+
+  describe("when asked to retrieve a non-existent administration") {
+    it("should return UserNotFound") {
+      withContainers { _ =>
+        for {
+          username <- Username("nonelena")
+        } do {
+          val result = AdministrationRepository.getInstance().getOrElse(fail()).findByUsername(username)
+          result shouldBe Left[ValidationError, Administration](UserNotFound)
         }
       }
     }
