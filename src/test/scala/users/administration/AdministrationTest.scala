f28efb42d6dbba1906abfe98ffeed66d550da128
turbo-ismam <turboismam@gmail.com>
2023-01-22 18:07:01 +0100

    fix: Administration creation test

--- src/test/scala/users/administration/AdministrationTest.scala
+++ src/test/scala/users/administration/AdministrationTest.scala
@@ -8,20 +8,26 @@ package io.github.pervasivecats
 package users.administration
 
 import io.github.pervasivecats.users.administration.entities.Administration
-
 import org.mockito.Mockito.description
 import org.mockito.Mockito.mock
 import org.mockito.Mockito.when
 import org.scalatest.funspec.AnyFunSpec
-
 import users.user.valueobjects.{EncryptedPassword, PlainPassword, Username}
+
+import org.scalatest.matchers.should.Matchers.shouldBe
+
+import scala.language.postfixOps
 
 class AdministrationTest extends AnyFunSpec {
 
-  describe("A administration") {
-    it("must be created correctly") {
-      val administration: Administration = Administration(Username("John").getOrElse(fail()))
+  val username: Username = Username("Elena").getOrElse(fail())
+
+  describe("An administration") {
+    describe("when created with a username"){
+      it("should contain them") {
+        val administration: Administration = Administration(username)
+        administration.username shouldBe username
+      }
     }
   }
-
 }
