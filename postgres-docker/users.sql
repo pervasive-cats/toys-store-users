f570df2ef782398ad71dea76482a9fe5cac79da2
Matteo Castellucci <matteo.castellucci@outlook.com>
2023-03-04 20:28:41 +0100

    refactor: refactor database generation sql script

--- postgres-docker/users.sql
+++ postgres-docker/users.sql
@@ -1,6 +1,7 @@
 CREATE TABLE public.administrators (
     username character varying(100) NOT NULL,
-    password character varying(60) NOT NULL
+    password character varying(60) NOT NULL,
+    CONSTRAINT administrators_pkey PRIMARY KEY (username)
 );
 
 CREATE TABLE public.customers (
@@ -8,17 +9,13 @@ CREATE TABLE public.customers (
     username character varying(100) NOT NULL,
     first_name character varying(100) NOT NULL,
     last_name character varying(100) NOT NULL,
-    password character varying(60) NOT NULL
+    password character varying(60) NOT NULL,
+    CONSTRAINT customers_pkey PRIMARY KEY (email)
 );
 
 CREATE TABLE public.store_managers (
     username character varying(100) NOT NULL,
     password character varying(60) NOT NULL,
-    store integer NOT NULL
+    store integer NOT NULL,
+    CONSTRAINT store_managers_pkey PRIMARY KEY (username)
 );
-
-ALTER TABLE ONLY public.administrators ADD CONSTRAINT administrators_pkey PRIMARY KEY (username);
-
-ALTER TABLE ONLY public.customers ADD CONSTRAINT customers_pkey PRIMARY KEY (email);
-
-ALTER TABLE ONLY public.store_managers ADD CONSTRAINT store_managers_pkey PRIMARY KEY (username);
